cfa7ad12c8c8e71f2b46bab0545089e41f73f8c3
maxkub <maxime.kubryk@gmail.com>
2015-12-29 00:51:26 +0100

    preprocessings almost done

--- challenge.py
+++ challenge.py
@@ -1,3 +1,10 @@
 from sklearn.metrics import roc_auc_score
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('data/train.csv', sep=';')
+
+print(df.head())
+
+print(df.dtypes)
